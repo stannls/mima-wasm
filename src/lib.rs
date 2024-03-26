2b2e4063ae8ec06a1df62dddf1dd32dd46d99afc
Yannis Storrer <yannis.storrer@web.de>
2024-03-26 15:53:29 +0100

    Removed initial sample code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
 #![feature(const_refs_to_static)]
 
-use wasm_bindgen::prelude::*;
 mod mima;
-
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello {}", name));
-}
